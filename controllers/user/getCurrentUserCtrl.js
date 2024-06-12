48e8b7e5a83d38d320e196932c1fee4e396975e6
Annka-Sarviro <annka_nuha@i.ua>
2024-06-12 10:52:06 +0300

    feat;add get Status rout

--- controllers/user/getCurrentUserCtrl.js
+++ controllers/user/getCurrentUserCtrl.js
@@ -1,6 +1,11 @@
 const gerCurrentUserCTRL = async (req, res) => {
   const user = req.user;
 
+  const { status } = req.query;
+  if (status === "getStatus") {
+    return res.status(200).json(user.status);
+  }
+
   return res.status(200).json(user);
 };
 
